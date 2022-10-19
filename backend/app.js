8c18d522fdd02e0229727ada2446cd2cef9717a1
charlotte deglaire <dgchacha28@gmail.com>
2022-10-19 02:21:54 +0200

    Ajout des routes dans le fichier app.js

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const path = require("path");
 const cors = require("cors");
+const sauceRoutes = require("./routes/sauce.js");
 const userRoutes = require("./routes/user.js");
 
 const app = express();
@@ -36,6 +37,7 @@ app.use(bodyParser.raw());
 app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "images")));
 
+app.use("/api/sauces", sauceRoutes);
 app.use("/api/auth", userRoutes);
 
 module.exports = app;
