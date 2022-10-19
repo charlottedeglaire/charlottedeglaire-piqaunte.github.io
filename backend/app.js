fe696a85caca5a6fccc0765590c91b79ecdd0829
charlotte deglaire <dgchacha28@gmail.com>
2022-10-19 02:08:14 +0200

    Ajout de bodyparser et de la route user.js dans le fichier app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
+require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
+const userRoutes = require("./routes/user.js");
 
 const app = express();
 mongoose
@@ -23,3 +29,12 @@ app.use((req, res, next) => {
     );
     next();
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+app.use(cors());
+
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
